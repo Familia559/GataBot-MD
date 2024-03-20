fb387726a3f676b289c41eb9a7421f889e158d04
MaguinhoMD <125109804+B4K123@users.noreply.github.com>
2024-03-20 07:46:10 -0300

    Update lista-lista.js

--- plugins/lista-lista.js
+++ plugins/lista-lista.js
@@ -19,5 +19,5 @@ const listMessage = {
   sections
 }
 await conn.sendMessage(m.chat, listMessage, {quoted: fkontak})}
-handler.command = /^listas|lists?$/i
+handler.command = /^listanegra|listamaguinho?$/i
 export default handler
