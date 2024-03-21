65d5146f27b05f23c0d86d9175b9a4157a11dc7b
MaguinhoMD <125109804+B4K123@users.noreply.github.com>
2024-03-21 15:14:28 -0300

    Update info-grupos-lista.js

--- plugins/info-grupos-lista.js
+++ plugins/info-grupos-lista.js
@@ -12,14 +12,14 @@ const participants = groupMetadata.participants || [];
 const bot = participants.find((u) => conn.decodeJid(u.id) === conn.user.jid) || {};
 const isBotAdmin = bot?.admin || false;
 const isParticipant = participants.some((u) => conn.decodeJid(u.id) === conn.user.jid);
-const participantStatus = isParticipant ? '✅ *ESTOU AQUÍ | YES*' : '❌ *NÃO ESTU AQUÍ | NO*';
+const participantStatus = isParticipant ? '✅ *ESTOY AQUÍ | YES*' : '❌ *NO ESTA AQUÍ | NO*';
 const totalParticipants = participants.length;
 txt += `🐈 *${i + 1}*
 ${lenguajeGB.smsLisC()} ${await conn.getName(jid)} ${participantStatus}
 ${lenguajeGB.smsLisD()} ${jid}
 ${lenguajeGB.smsLisF()} ${isBotAdmin ? '✅' : '❌'}
 ${lenguajeGB.smsLisE()}  ${totalParticipants}
-${lenguajeGB.smsLisG()} ${isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(jid) || 'Error'}` : 'NÃO SOU ADMINISTRADOR'}\n\n`;
+${lenguajeGB.smsLisG()} ${isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(jid) || 'Error'}` : 'NO SOY ADMINISTRADOR'}\n\n`;
 }
 m.reply(`${packname} ${lenguajeGB.smsLisA()}
 ${lenguajeGB.smsLisB()} ${totalGroups}\n\n${txt}`.trim());
@@ -33,21 +33,22 @@ const participants = groupMetadata.participants || [];
 const bot = participants.find((u) => conn.decodeJid(u.id) === conn.user.jid) || {};
 const isBotAdmin = bot?.admin || false;
 const isParticipant = participants.some((u) => conn.decodeJid(u.id) === conn.user.jid);
-const participantStatus = isParticipant ? '✅ *ESTOU AQUÍ | YES*' : '❌ *NÃO ESTA AQUÍ | NO*';
+const participantStatus = isParticipant ? '✅ *ESTOY AQUÍ | YES*' : '❌ *NO ESTA AQUÍ | NO*';
 const totalParticipants = participants.length;    
     txt += `🐈 ${i + 1}
 ${lenguajeGB.smsLisC()} ${await conn.getName(jid)} ${participantStatus}
 ${lenguajeGB.smsLisD()} ${jid}
 ${lenguajeGB.smsLisF()} ${isBotAdmin ? '✅' : '❌'}
 ${lenguajeGB.smsLisE()} ${totalParticipants}
-${lenguajeGB.smsLisG()} ${isBotAdmin ? 'Error' : 'NÃO SOU ADMINISTRADOR'}\n\n`
+${lenguajeGB.smsLisG()} ${isBotAdmin ? 'Error' : 'NO SOY ADMINISTRADOR'}\n\n`
 }
 m.reply(`${packname} ${lenguajeGB.smsLisA()}
 ${lenguajeGB.smsLisB()} ${totalGroups}\n\n${txt}`.trim());
 }}
-handler.help = ['groups', 'grupolista']
+handler.help = ['groups', 'grouplist']
 handler.tags = ['info']
-handler.command = /^(groups|grupolista|listadegrupo|gruposlista|listagrupos|listadegrupos|grupolista|listagrupo)$/i
+handler.command = /^(groups|grouplist|listadegrupo|gruposlista|listagrupos|listadegrupos|grupolista|listagrupo)$/i
 handler.exp = 5
 handler.register = true
 export default handler
+  
