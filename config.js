0e1000879a60c70fbf4c65d891797ba2e90de166
Familia559 <164744484+Familia559@users.noreply.github.com>
2024-03-24 08:45:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["558193813503", '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
-["558193813503"], 
-["558193813503"], ["558193813503"], ["558193813503"], ["558193813503"]]
+["5527998925820", '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
+["5527998925820"], 
+["5527998925820"], ["5527998925820"], ["5527998925820"], ["5527998925820"]]
 
 global.mods = []
 global.prems = []
